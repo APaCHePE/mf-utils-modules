8dce4c2b76032ac0171734ea7e5ffd94cd819df4
Apachepe <alexanderspradochoquepata@gmail.com>
2025-05-27 00:20:50 -0500

    Initial commit

---
+++ src/test-mf-utils-modules.js
@@ -0,0 +1,9 @@
+// Anything exported from this file is importable by other in-browser modules.
+export const isSidebarMenuItemsVisible = (roles) => {
+  console.log("roles", roles);
+  const currentRole = localStorage.getItem("role");
+  console.log("currentRole", currentRole);
+  console.log("MF-UTILS-MODULES");
+
+  return roles.includes(currentRole);
+};
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "test",
+    projectName: "mf-utils-modules",
+    webpackConfigEnv,
+    argv,
+    outputSystemJS: true,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
