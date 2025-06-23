fee3ee4ce0f79791abccd81f55223b4c3ae9eadc
Alex Prado <alexanders.prado@interseguro.com.pe>
2025-06-23 18:18:52 -0500

    Changes

--- src/api/menu/options.api.js
+++ src/api/menu/options.api.js
@@ -2,8 +2,8 @@ import http from '../config/http-client';
 
 export const getUserOptions = async (userId, token) => {
   console.log(`Fetching user options for userId: ${userId}, token: ${token}` );
-  
-  const response = await http.get(`http://localhost:3001/api/users/options?idUsuario=${userId}`, {
+
+  const response = await http.get(`https://gateway8079.emapasalas.net.pe/options?idUsuario=${userId}`, {
     headers: {
       'Authorization': `Bearer ${token}`,
       'Content-Type': 'application/json',
